1950d8fa20ee716ae3425bec28c0d178e80e13fa
Jasonbluefire <Jasonbluefire@gmail.com>
2025-06-14 00:50:15 -0400

    One more _ instead of -
    One more _ instead of -

--- custom_components/purpleair-limitsensors/const.py
+++ custom_components/purpleair-limitsensors/const.py
@@ -8,6 +8,6 @@ from homeassistant.const import Platform
 LOGGER: Final = logging.getLogger(__package__)
 PLATFORMS: Final = [Platform.SENSOR]
 
-DOMAIN: Final[str] = "purpleair-lite"
+DOMAIN: Final[str] = "purpleair_lite"
 
 CONF_SENSOR_INDICES: Final[str] = "sensor_indices"
